b9bfd9c6fbf82f723d7067e1185d388e61c7820e
Laura Kirby <laurakirby26@gmail.com>
2017-03-14 14:32:24 -0700

    add chapter test from old version of book

---
+++ wk_7/SelfTestCh7.java
@@ -0,0 +1,32 @@
+// 1. Does a superclass have access to the members of a subclass? Does a subclass have access to the members of a superclass?
+//
+// 2. Create a subclass of TwoDShape called Circle.
+// Include an area( ) method that computes the area of the circle and a constructor that uses super to initialize the TwoDShape portion.
+//
+// 3. How do you prevent a subclass from having access to a member of a superclass?
+//
+// 4. Describe the purpose and use of both versions of super.
+//
+// 5. Given the following hierarchy:
+//    class Alpha { ... class Beta extends Alpha { ... Class Gamma extends Beta { ...In
+//     what order are the constructors for these classes called when a Gamma object is instantiated?
+//
+// 6. A superclass reference can refer to a subclass object. Explain why this is important as it relates to method overriding.
+//
+// 7. What is an abstract class?
+//
+// 8. How do you prevent a method from being overridden? How do you prevent a class from being inherited?
+//
+// 9. Explain how inheritance, method overriding, and abstract classes are used to support polymorphism.
+//
+// 10. What class is a superclass of every other class?
+//
+// 11. A class that contains at least one abstract method must, itself, be declared abstract. True or False?
+//
+// 12. What keyword is used to create a named constant?
+//
+// APA (American Psychological Assoc.)
+// Schildt, H. (2005). Java : A Beginner's Guide. New York: McGraw-Hill Professional.
+//
+// MLA (Modern Language Assoc.)
+// Schildt, Herbert. Java : A Beginner's Guide. vol. 3rd ed, McGraw-Hill Professional, 2005. EBSCOhost.
