b2f57c198a779b63b7193d05f9d451b6d1fa9956
Laura Kirby <laurakirby26@gmail.com>
2017-03-20 17:18:43 -0700

    chapter 7 homework and quiz

--- wk_7/InheritanceHwSeven.java
+++ wk_7/InheritanceHwSeven.java
@@ -6,11 +6,13 @@
     java InheritanceHwSeven
 
   Application description:
-    - Write a program in which you create a class,
-      a subclass of that class, and a demo class with main.
-      - The subclass must:
-        1. override a method inherited from its immediate superclass and
-        2. use the keywords this and super to make use of both versions of the overridden method
+    - Write a program in which you create a
+      1. class
+      2. subclass of that class
+          a. override a method inherited from its immediate superclass and
+          b. use the this keyword to make use of the local version of the overridden method
+          c. use the super keyword to make use of the super version of the overridden method
+      3. demo class with main.
 
     See the HouseBoat class and build method in lesson 7 class notes.
 
@@ -47,3 +49,83 @@
 
 */
 
+// 1. class
+class Plant
+{
+  // 5. class variable (1)
+  private static String[] primitiveTypes = new String[] {"fern", "moss"};
+  static String cholorplast = "I contain chlorophyll, a green pigment that absorbs light energy for photosynthesis";
+
+  // 4. instance variable, also known as a field (1)
+  String name;
+  int cellCount;
+
+  // 2. parameterized constructor (1)
+  Plant(String name, int cellCount)
+  {
+    this.name = name;
+    this.cellCount = cellCount;
+  }
+
+  Integer calculateChloroplast () {
+    Integer chloroplastRatio = 100;
+    Integer totalCholoroplast = (this.cellCount * chloroplastRatio);
+    System.out.println("------------- \n Method belonging to the 'super' class was called. \n There are about " + chloroplastRatio + " cholorplasts per cell. \n Returning: " + totalCholoroplast + "\n -------------");
+    return totalCholoroplast;
+  }
+}
+
+// 2. a subclass
+class Magnolia extends Plant
+{
+  static boolean doesBloom = true;
+  static boolean hasLeaves = true;
+  static boolean hasRoots = true;
+
+  String flowerColor;
+  Integer waterLevel;
+
+
+  Magnolia(String plantName, Integer plantCellCount, String flowerColor, Integer waterLevel)
+  {
+    super(plantName, plantCellCount);
+    this.flowerColor = flowerColor;
+    this.waterLevel = waterLevel;
+  }
+
+  // a. override a method inherited from its immediate superclass
+  Integer calculateChloroplast () {
+    Integer chloroplastRatio = 100;
+    Integer totalCholoroplast = (cellCount * chloroplastRatio) + 25;
+    System.out.println("------------- \n Override 'calculateChloroplast' \n Returning: " + totalCholoroplast + "\n -------------");
+    return totalCholoroplast;
+  }
+
+  // b. use the this keyword to make use of the local version of the overridden method
+  String printThisCholoroplast (){
+    Integer result = this.calculateChloroplast();
+    System.out.println("------------- \n Call class method using keyword 'this'. \n calculateChloroplast " + result + "\n -------------");
+    String stringResult = Integer.toString(result);
+    return stringResult;
+  }
+
+  // c. use the super keyword to make use of the super version of the overridden method
+  String printSuperCholoroplast (){
+    Integer result = super.calculateChloroplast();
+    System.out.println("------------- \n Call super class method using keyword 'super' \n 'calculateChloroplast' " + result + "\n -------------");
+    String stringResult = Integer.toString(result);
+    return stringResult;
+  }
+}
+
+// 3. demo class with main.
+class InheritanceHwSeven
+{
+  public static void main(String[] args)
+  {
+    Magnolia aMagnolia = new Magnolia("Stellata", 540, "white", 4);
+    aMagnolia.calculateChloroplast();
+    aMagnolia.printThisCholoroplast();
+    aMagnolia.printSuperCholoroplast();
+  }
+}
