a93d293b13b948ea391716fdd40a92e75a258372
Laura Kirby <laurakirby26@gmail.com>
2017-04-11 10:01:20 -0700

    finish hw 10 and quiz

--- 170405_wk_10/JavaPackagesHwTen.java
+++ 170405_wk_10/JavaPackagesHwTen.java
@@ -15,15 +15,16 @@
       - compare two files and indicate the first location where the two files differ
       - capture the source code of a web page URL
 */
+
 import java.io.*;
 import java.util.*;
 
 class LKUtility {
-  String docText = "Anna decided to spend the civic holiday on a kayak adventure.\n At noon, she found out that her mom waw out driving a racecar.";
+  String docText = "Anna decided to spend the civic holiday on a kayak adventure.\n At noon she found out that her mom was out driving a racecar";
   PrintWriter writer;
   String file = "./story.txt";
 
-  void writeStory(){
+  void writeStory() {
     try{
       this.writer = new PrintWriter(file, "UTF-8");
       this.writer.println(this.docText);
@@ -34,28 +35,32 @@ class LKUtility {
     }
   }
 
-  String printPalindromes(){
+  String[] printPalindromes() {
     String[] words = docText.split(" ");
+    String[] palindromes;
     int count = 0;
+    String result = "";
 
-    for(String s : words)
-    {
+    for(String s : words) {
       String eachWord = s.toLowerCase();
       String reverse = new StringBuffer(s).reverse().toString().toLowerCase();
 
-      if (eachWord.length() > 1 && eachWord.equals(reverse)) {
+      if(eachWord.length() > 1 && eachWord.equals(reverse)) {
         System.out.println(eachWord);
         count += 1;
+        result += eachWord += " ";
       }
     }
+
     System.out.println("there are " + count + " palindromes");
-    return "Anna";
+    palindromes = result.split(" ");
+    return palindromes;
   }
 }
 
 class JavaPackagesHwTen {
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
     LKUtility aCustomUtility = new LKUtility();
     aCustomUtility.writeStory();
     aCustomUtility.printPalindromes();
